bf2f130bc7470122c5299a87cbddaa44fd456eef
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-23 15:37:52 -0300

    Bug no process.env.CLEARDB_DATABASE_URL / problema de timeout desconhecido

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -23,20 +23,22 @@ function createDBConnection(){
     }
 
     if(process.env.NODE_ENV == 'production'){
-        var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
-        var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+        // bug no process.env.CLEARDB_DATABASE_URL
+        //var urlDeConexao = process.env.CLEARDB_DATABASE_URL;
+        //console.log(urlDeConexao);
+       // var grupos = urlDeConexao.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
         /*
             [0] - RegEx completa
             [1] - login
             [2] - senha
             [3] - host
             [4] - db
-        */
+        */        
         return mysql.createConnection({
-            host : grupos[3],
-            user : grupos[1],
-            password : grupos[2],
-            database : grupos[4]
+            host: '...',
+            user:'...',
+            password:'...',
+            database:'...'
         });
     }
 }
